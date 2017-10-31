cf03500f711bce2314513fe1bc0550616d978881
uics15 <yunfan-jiang@uiowa>
2017-10-30 21:03:53 -0500

    History and data

---
+++ GlobalAntiAllergy/ProductsInfo.swift
@@ -0,0 +1,14 @@
+//
+//  ProductsInfo.swift
+//  GlobalAntiAllergy
+//
+//  Created by uics15 on 10/28/17.
+//  Copyright © 2017 uics15. All rights reserved.
+//
+
+import Foundation
+
+class ProductsInfo {
+    
+    
+}
--- GlobalAntiAllergy/ViewController.swift
+++ GlobalAntiAllergy/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
+struct ProductScanned{
+    let product_name_en:String
+    let image_front_small_url:String
+    let ingredients_text_with_allergens_en: String
+    
+}
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let jsonUrlString = "https://world.openfoodfacts.org/api/v0/product/737628064502.json"
+        guard let url = URL(string:jsonUrlString) else
+        {return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            //let dataasString = String(data: data, encoding:.utf8)
+            //print (dataasString)
+            do {
+                let json = try JSONSerialization.jsonObject(with:data, options:.mutableContainers)
+            print (json)
+            } catch let jsonErr {
+                print ("error serializinf json:", jsonErr)
+            }
+        }.resume()
     }
 
     override func didReceiveMemoryWarning() {
